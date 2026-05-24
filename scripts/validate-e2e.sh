cfa4eecadc3450c88b7b37951a07e745343af758
ghazi dayah <ghazidayeh01@gmail.com>
2026-05-24 01:27:07 +0100

    Fix validate-e2e log checks after F1 cleanup

--- scripts/validate-e2e.sh
+++ scripts/validate-e2e.sh
@@ -20,6 +20,6 @@ kubectl -n oran-ran exec "$UE_POD" -- sh -c 'ip route'
 kubectl -n oran-ran exec "$UE_POD" -- ping -c 2 10.45.0.1
 kubectl -n oran-ran exec "$UE_POD" -- ping -I oaitun_ue1 -c 4 8.8.8.8
 
-kubectl -n oran-core logs deploy/open5gs-amf --since=10m | egrep -i 'Registration complete|gNB|999700000000001'
-kubectl -n oran-core logs deploy/open5gs-smf --since=10m | egrep -i 'UE SUPI|session|10.45|oai|associated'
-kubectl -n oran-core logs "$UPF_POD" --since=10m | egrep -i 'gtp|2152|PFCP|associated'
+kubectl -n oran-core logs deploy/open5gs-amf --since=30m | egrep -i 'Registration complete|gNB|999700000000001' || true
+kubectl -n oran-core logs deploy/open5gs-smf --since=30m | egrep -i 'UE SUPI|session|10.45|oai|associated' || true
+kubectl -n oran-core logs "$UPF_POD" --since=30m | egrep -i 'gtp|2152|PFCP|associated' || true
